acb3a23f3e924a25a590f36fdb6298ee398a4acd
Avanthi <88855693+avanthicodes@users.noreply.github.com>
2023-03-16 19:24:51 -0500

    Register/Login Pages

---
+++ fnf/lib/login.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'FriendNFlix';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        backgroundColor: const Color(0xFFB20710), // set background to the shade of red here
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.28, // move everything down by 20%
+            ),
+
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'LOGIN',
+                style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 32,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white),
+              ),
+            ),
+
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  labelText: 'Enter Username',
+                  labelStyle: const TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                  filled: true,
+                  fillColor: Color.fromRGBO(255, 255, 255, 0.18), // 18% opacity white
+                ),
+              ),
+            ),
+
+
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  labelText: 'Enter Password',
+                  labelStyle: const TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                  filled: true,
+                  fillColor: Color.fromRGBO(255, 255, 255, 0.18), // 18% opacity white
+                ),
+              ),
+            ),
+
+            Center(
+              child: Transform.translate(
+                offset: Offset(0, MediaQuery.of(context).size.height * 0.05),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.27,
+                  height: 47,
+                  child: ElevatedButton(
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xFF000000),
+                    ),
+                    onPressed: () {
+                      print(nameController.text);
+                      print(passwordController.text);
+                    },
+                  ),
+                ),
+              ),
+            ),
+
+            Transform.translate(
+              offset: Offset(0, MediaQuery.of(context).size.height * 0.06),
+              child: Row(
+                children: <Widget>[
+                  TextButton(
+                    child: const Text(
+                      'NEW ACCOUNT? REGISTER HERE',
+                      style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.white,
+                          fontStyle: FontStyle.italic),
+                    ),
+                    onPressed: () {
+                      //signup screen
+                    },
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
